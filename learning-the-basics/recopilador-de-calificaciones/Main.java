b725f91aa5d3e8bb1e5531eaf1ecfb29dbe4670d
AnotherMizu <otrorandomas7@gmail.com>
2026-09-09 14:42:53 -0600

    refactor: limpiar variables sin uso y estructurar historial con HashMap anidado

--- learning-the-basics/recopilador-de-calificaciones/Main.java
+++ learning-the-basics/recopilador-de-calificaciones/Main.java
@@ -7,63 +7,94 @@ public class Main {
 
     public static void main(String[] args){
 
-        HashMap<String, Integer> calificaciones = new HashMap<>();
+        HashMap<Integer, HashMap<String, Integer>> historial = new HashMap<>();
         boolean validation = true;
         float promedio = 0;
-        int sumaCalificaciones = 0;
-        int calificacionMasAlta = 0;
-        int calificacionMasBaja = 0;
-        int[] calfNotorias = new int[2];
 
         Scanner sc = new Scanner(System.in);
 
+        int numeroUnidad = 1;
+
         while (validation == true){
 
+            HashMap<String, Integer> calificacionesUnidad = new HashMap<>();
+
             System.out.println("¿Cuantas materias tienes?");
-            int cantMaterias = sc.nextInt();    
-            
+            int cantMaterias = sc.nextInt();
+
             sc.nextLine();
 
-            promedio = userInput(cantMaterias, sc, promedio, calificaciones);
+            promedio = userInput(cantMaterias, sc, calificacionesUnidad);
+
             System.out.println(promedio);
 
-            showResults(calificaciones, promedio);
+            historial.put(numeroUnidad, calificacionesUnidad);
+
+            showResults(calificacionesUnidad, promedio);
 
             String userAnswer = sc.nextLine();
+
             if (userAnswer.equals("no")){
                 validation = false;
             }
+
+            numeroUnidad++;
+
         }
+
         sc.close();
     }
 
-    public static int[] notorias(HashMap<String, Integer> calificaciones) {  
+
+    public static int[] notorias(HashMap<String, Integer> historial) {
+
         int[] calfNotorias = new int[2];
-        calfNotorias[0] = Collections.min(calificaciones.values());
-        calfNotorias[1] = Collections.max(calificaciones.values());
+
+        calfNotorias[0] = Collections.min(historial.values());
+        calfNotorias[1] = Collections.max(historial.values());
 
         return calfNotorias;
     }
-    public static void showResults(HashMap<String, Integer> calificaciones, float promedio){
 
-        int[] cantidades = Main.notorias(calificaciones);
-        System.out.println("Tus promedios en esta unidad son de: " + promedio);
-        System.out.println("Tus calificaciones más altas y más bajas son: " + Arrays.toString(cantidades));
+
+    public static void showResults(HashMap<String, Integer> calificacionesUnidad, float promedio){
+
+        int[] cantidades = Main.notorias(calificacionesUnidad);
+
+        System.out.println(
+            "Tus promedios en esta unidad son de: " + promedio
+        );
+
+        System.out.println(
+            "Tus calificaciones más altas y más bajas son: "
+            + Arrays.toString(cantidades)
+        );
+
         System.out.println("Do you want to try it again?");
-   
-    };
-    public static float userInput(float cantMaterias,  Scanner sc, float promedio, HashMap<String, Integer> calificaciones ){
+    }
+
+
+    public static float userInput(int cantMaterias, Scanner sc, HashMap<String, Integer> calificacionesUnidad){
+
         float sumaCalificaciones = 0;
+
         for (int i = 1; i <= cantMaterias; i += 1) {
+
             System.out.println("Cual es tu " + i + " materia");
             String materia = sc.nextLine();
+
             System.out.println("Cuanto sacaste?");
             int calificacion = sc.nextInt();
+
             sc.nextLine();
-            sumaCalificaciones += calificacion; 
-            calificaciones.put(materia, calificacion);
+
+            sumaCalificaciones += calificacion;
+
+            calificacionesUnidad.put(materia, calificacion);
         }
-        promedio = sumaCalificaciones/cantMaterias;
-        return promedio; 
+
+        float promedio = sumaCalificaciones / cantMaterias;
+
+        return promedio;
     }
 }
